eb4541b3c35bb8a36c65c511e713b1c6ac266151
Douglas Neves <d88.neves@gmail.com>
2021-01-10 23:00:27 -0300

    Dogs Project
    Dogs Project

---
+++ dogs/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Api from './api/Api';
+
+const App = () => {
+  return <div><Api /></div>;
+};
+
+export default App;
---
+++ dogs/src/api/endpoints/UserPost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserPost() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserPost
