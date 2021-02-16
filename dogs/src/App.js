e43e123c0f5835d715b6ff380a3822a167bf89e4
Douglas Neves <d88.neves@gmail.com>
2021-02-15 22:33:35 -0300

    Update
    Update

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Api from './api/Api';
 
-const App = () => {
-  return <div><Api /></div>;
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
