7c75212719996521a3bf49e9c9ec5a4d70a904f2
Douglas Neves <d88.neves@gmail.com>
2021-12-12 21:30:03 -0300

    Update
    Update

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
-    <div>pagina</div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
--- dogs/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Api = () => {
-  return <div>
-    Minha div
-  </div>;
-};
-
-export default Api;
